0676dd654defeb3471b0e98355daa5f0242e2b0c
Kushagra-0 <kushagrashukla.dev@gmail.com>
2025-05-05 17:14:08 +0530

    incorrect verify otp page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home/Home';
 import Register from './pages/register/Register';
 import Login from './pages/login/Login';
+import VerifyOtp from './pages/verifyOtp/VerifyOtp';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/verifyOtp" element={<VerifyOtp />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/verifyOtp/VerifyOtp.tsx
@@ -0,0 +1,12 @@
+import VerifyForm from "../../components/auth/VerifyForm";
+
+
+const VerifyOtp = () => {
+  return (
+    <div className="bg-gradient-to-r from-blue-200 to-pink-200 p-8 h-screen w-full flex items-center justify-center">
+      <VerifyForm />
+    </div>
+  );
+};
+
+export default VerifyOtp;
