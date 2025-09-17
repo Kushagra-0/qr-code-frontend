d2201bd221cc0ee61095f282712bdc3f03ffb322
Kushagra-0 <kushgarashukla.dev@gmail.com>
2025-09-17 22:45:45 +0530

    updated baseurl

--- src/common/constant.ts
+++ src/common/constant.ts
@@ -1,2 +1,3 @@
-export const baseUrl = "http://localhost:3010/api"
+export const baseUrl = "http://13.201.31.129:3010/api"
+// export const baseUrl = "http://localhost:3010/api"
 // export const baseUrl = "https://p94j3td9-3010.inc1.devtunnels.ms/api"
